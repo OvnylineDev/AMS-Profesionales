6e4d4b7b70ac4e26aad46df7d696006a018bf1b9
OvnylineDev <ovnylinedesarrollo@gmail.com>
2018-04-04 13:54:28 +0200

    Revert "First Commit"
    This reverts commit 43a9997b12cce5f763685eba46e7a604e83ac951.

--- server/otros/data/dbms.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /*
-     * SOURCE: MS SQL
-     */
-    define('MSSQL_HOST','OVNYLINE04\SQLEXPRESS'); /*Enlace*/
-    define('MSSQL_USER','Ovnyline'); /*Usuario*/
-    define('MSSQL_PASSWORD','AMS2016ovnyline');
-    define('MSSQL_DATABASE','GestionExpedientes');  
-?>
