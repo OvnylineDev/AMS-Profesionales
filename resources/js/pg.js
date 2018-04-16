82d2694a8efc6dd8f84a3b83dd067b17395f180b
OvnylineDev <ovnylinedesarrollo@gmail.com>
2018-04-16 17:41:28 +0200

    Back

--- resources/js/pg.js
+++ resources/js/pg.js
@@ -15,73 +15,71 @@ function onBackKeyDown() {
      // alert(window.location.pathname);
      // var url = window.location.pathname;
      // var filename = url.substring(url.lastIndexOf('/')+1);
-     alert(url);
-     //
-     // switch (location) {
-     //      case "index.html":
-     //           alert(location);
-     //           // navigator.app.exitApp();
-     //      break;
-     //      case "principal.html":
-     //           alert(location);
-     //      break;
-     //      case "listado.html":
-     //           alert(location);
-     //      break;
-     //      case "menup.html":
-     //           alert(location);
-     //      break;
-     //      case "llamada.html":
-     //           alert(location);
-     //      break;
-     //      case "historial.html":
-     //           alert(location);
-     //      break;
-     //      case "seguimiento.html":
-     //           alert(location);
-     //      break;
-     //      case "foto.html":
-     //           alert(location);
-     //      break;
-     //      case "planos.html":
-     //           alert(location);
-     //      break;
-     //      case "parte.html":
-     //           alert(location);
-     //      break;
-     //      case "fincita.html":
-     //           alert(location);
-     //      break;
-     //      case "val-foto.html":
-     //           alert(location);
-     //      break;
-     //      case "val-general.html":
-     //           alert(location);
-     //      break;
-     //      case "val-texto.html":
-     //           alert(location);
-     //      break;
-     //      case "fest-eligeparte.html":
-     //           alert(location);
-     //      break;
-     //      case "fest-foto.html":
-     //           alert(location);
-     //      break;
-     //      case "fest-general.html":
-     //           alert(location);
-     //      break;
-     //      case "fest-historial.html":
-     //           alert(location);
-     //      break;
-     //      case "fest-parte.html":
-     //           alert(location);
-     //      break;
-     //      case "fest-planos.html":
-     //           alert(location);
-     //      break;
-     //      default:
-     //
-     // }
+
+     switch (url) {
+          case "index.html":
+               navigator.app.exitApp();
+          break;
+          case "principal.html":
+               window.location.href = "index.html";
+          break;
+          case "listado.html":
+               window.location.href = "principal.html" + "?token=" + token;
+          break;
+          case "menup.html":
+               window.location.href = "principal.html" + "?token=" + token;
+          break;
+          case "llamada.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "historial.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "seguimiento.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "foto.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "planos.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "parte.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "fincita.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "val-general.html":
+               window.location.href = "menup.html" + "?token=" + token;
+          break;
+          case "val-foto.html":
+               alert(url);
+          break;
+          case "val-texto.html":
+               alert(url);
+          break;
+          case "fest-eligeparte.html":
+               alert(url);
+          break;
+          case "fest-foto.html":
+               alert(url);
+          break;
+          case "fest-general.html":
+               alert(url);
+          break;
+          case "fest-historial.html":
+               alert(url);
+          break;
+          case "fest-parte.html":
+               alert(url);
+          break;
+          case "fest-planos.html":
+               alert(url);
+          break;
+          default:
+
+     }
 }
 
 function getURL() {
