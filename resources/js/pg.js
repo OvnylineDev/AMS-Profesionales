4e1cf004a95a0d0453e99414736167189d1d8973
OvnylineDev <ovnylinedesarrollo@gmail.com>
2018-04-16 16:54:59 +0200

    Test deviceready

--- resources/js/pg.js
+++ resources/js/pg.js
@@ -1,18 +1,18 @@
-// //events
-// // Wait for device API libraries to load
-// //
-// document.addEventListener("deviceready", phonegapReady, false);
+//events
+// Wait for device API libraries to load
 //
-// function phonegapReady(){
-//      document.addEventListener("backbutton", onBackKeyDown, false);
-// }
-//
-// function onBackKeyDown() {
-//      // alert(document.URL);
-//      // alert(location.pathname);
-//      // alert(window.location.pathname);
-//
-//      var url = window.location.pathname;
-//      var filename = url.substring(url.lastIndexOf('/')+1);
-//      alert(filename);
-// }
+document.addEventListener("deviceready", phonegapReady, false);
+
+function phonegapReady(){
+     document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+function onBackKeyDown() {
+     // alert(document.URL);
+     // alert(location.pathname);
+     // alert(window.location.pathname);
+
+     var url = window.location.pathname;
+     var filename = url.substring(url.lastIndexOf('/')+1);
+     alert(filename);
+}
