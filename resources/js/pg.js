f56882209b1158089b165d46f90f7054e8acee76
OvnylineDev <ovnylinedesarrollo@gmail.com>
2018-04-16 17:27:43 +0200

    (location)

--- resources/js/pg.js
+++ resources/js/pg.js
@@ -1,91 +1,91 @@
-// var location = "";
-// //events
-// // Wait for device API libraries to load
-//
-// document.addEventListener("deviceready", phonegapReady, false);
-//
-// function phonegapReady(){
-//      location = getURL();
-//      document.addEventListener("backbutton", onBackKeyDown, false);
-// }
-//
-// function onBackKeyDown() {
-//      // alert(document.URL);
-//      // alert(location.pathname);
-//      // alert(window.location.pathname);
-//      // var url = window.location.pathname;
-//      // var filename = url.substring(url.lastIndexOf('/')+1);
-//      alert(filename);
-//      //
-//      // switch (location) {
-//      //      case "index.html":
-//      //           alert(filename);
-//      //           // navigator.app.exitApp();
-//      //      break;
-//      //      case "principal.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "listado.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "menup.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "llamada.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "historial.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "seguimiento.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "foto.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "planos.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "parte.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "fincita.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "val-foto.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "val-general.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "val-texto.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "fest-eligeparte.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "fest-foto.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "fest-general.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "fest-historial.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "fest-parte.html":
-//      //           alert(filename);
-//      //      break;
-//      //      case "fest-planos.html":
-//      //           alert(filename);
-//      //      break;
-//      //      default:
-//      //
-//      // }
-// }
-//
-// function getURL() {
-//      var url = window.location.pathname;
-//      var filename = url.substring(url.lastIndexOf('/')+1);
-//      return filename;
-// }
+var location = "";
+//events
+// Wait for device API libraries to load
+
+document.addEventListener("deviceready", phonegapReady, false);
+
+function phonegapReady(){
+     var location = getURL();
+     document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+function onBackKeyDown() {
+     // alert(document.URL);
+     // alert(location.pathname);
+     // alert(window.location.pathname);
+     // var url = window.location.pathname;
+     // var filename = url.substring(url.lastIndexOf('/')+1);
+     alert(location);
+     //
+     // switch (location) {
+     //      case "index.html":
+     //           alert(location);
+     //           // navigator.app.exitApp();
+     //      break;
+     //      case "principal.html":
+     //           alert(location);
+     //      break;
+     //      case "listado.html":
+     //           alert(location);
+     //      break;
+     //      case "menup.html":
+     //           alert(location);
+     //      break;
+     //      case "llamada.html":
+     //           alert(location);
+     //      break;
+     //      case "historial.html":
+     //           alert(location);
+     //      break;
+     //      case "seguimiento.html":
+     //           alert(location);
+     //      break;
+     //      case "foto.html":
+     //           alert(location);
+     //      break;
+     //      case "planos.html":
+     //           alert(location);
+     //      break;
+     //      case "parte.html":
+     //           alert(location);
+     //      break;
+     //      case "fincita.html":
+     //           alert(location);
+     //      break;
+     //      case "val-foto.html":
+     //           alert(location);
+     //      break;
+     //      case "val-general.html":
+     //           alert(location);
+     //      break;
+     //      case "val-texto.html":
+     //           alert(location);
+     //      break;
+     //      case "fest-eligeparte.html":
+     //           alert(location);
+     //      break;
+     //      case "fest-foto.html":
+     //           alert(location);
+     //      break;
+     //      case "fest-general.html":
+     //           alert(location);
+     //      break;
+     //      case "fest-historial.html":
+     //           alert(location);
+     //      break;
+     //      case "fest-parte.html":
+     //           alert(location);
+     //      break;
+     //      case "fest-planos.html":
+     //           alert(location);
+     //      break;
+     //      default:
+     //
+     // }
+}
+
+function getURL() {
+     var url = window.location.pathname;
+     var filename = url.substring(url.lastIndexOf('/')+1);
+     return filename;
+}
