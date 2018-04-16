8af7b58f2f4a225ba274a43641a45d3f70577f1b
OvnylineDev <ovnylinedesarrollo@gmail.com>
2018-04-16 17:15:21 +0200

    Test Errors

--- resources/js/pg.js
+++ resources/js/pg.js
@@ -15,71 +15,71 @@ function onBackKeyDown() {
      // alert(window.location.pathname);
      // var url = window.location.pathname;
      // var filename = url.substring(url.lastIndexOf('/')+1);
-     // alert(filename);
+     alert(filename);
      //
-     switch (location) {
-          case "index.html":{
-               alert(filename);
-               // navigator.app.exitApp();
-          }break;
-          case "principal.html":{
-               alert(filename);
-          }break;
-          case "listado.html":{
-               alert(filename);
-          }break;
-          case "menup.html":{
-               alert(filename);
-          }break;
-          case "llamada.html":{
-               alert(filename);
-          }break;
-          case "historial.html":{
-               alert(filename);
-          }break;
-          case "seguimiento.html":{
-               alert(filename);
-          }break;
-          case "foto.html":{
-               alert(filename);
-          }break;
-          case "planos.html":{
-               alert(filename);
-          }break;
-          case "parte.html":{
-               alert(filename);
-          }break;
-          case "fincita.html":{
-               alert(filename);
-          }break;
-          case "val-foto.html":{
-               alert(filename);
-          }break;
-          case "val-general.html":{
-               alert(filename);
-          }break;
-          case "val-texto.html":{
-               alert(filename);
-          }break;
-          case "fest-eligeparte.html":{
-               alert(filename);
-          }break;
-          case "fest-foto.html":{
-               alert(filename);
-          }break;
-          case "fest-general.html":{
-               alert(filename);
-          }break;
-          case "fest-historial.html":{
-               alert(filename);
-          }break;
-          case "fest-parte.html":{
-               alert(filename);
-          }break;
-          case "fest-planos.html":{
-               alert(filename);
-          }break;
-          default:
-
-     }
+     // switch (location) {
+     //      case "index.html":
+     //           alert(filename);
+     //           // navigator.app.exitApp();
+     //      break;
+     //      case "principal.html":
+     //           alert(filename);
+     //      break;
+     //      case "listado.html":
+     //           alert(filename);
+     //      break;
+     //      case "menup.html":
+     //           alert(filename);
+     //      break;
+     //      case "llamada.html":
+     //           alert(filename);
+     //      break;
+     //      case "historial.html":
+     //           alert(filename);
+     //      break;
+     //      case "seguimiento.html":
+     //           alert(filename);
+     //      break;
+     //      case "foto.html":
+     //           alert(filename);
+     //      break;
+     //      case "planos.html":
+     //           alert(filename);
+     //      break;
+     //      case "parte.html":
+     //           alert(filename);
+     //      break;
+     //      case "fincita.html":
+     //           alert(filename);
+     //      break;
+     //      case "val-foto.html":
+     //           alert(filename);
+     //      break;
+     //      case "val-general.html":
+     //           alert(filename);
+     //      break;
+     //      case "val-texto.html":
+     //           alert(filename);
+     //      break;
+     //      case "fest-eligeparte.html":
+     //           alert(filename);
+     //      break;
+     //      case "fest-foto.html":
+     //           alert(filename);
+     //      break;
+     //      case "fest-general.html":
+     //           alert(filename);
+     //      break;
+     //      case "fest-historial.html":
+     //           alert(filename);
+     //      break;
+     //      case "fest-parte.html":
+     //           alert(filename);
+     //      break;
+     //      case "fest-planos.html":
+     //           alert(filename);
+     //      break;
+     //      default:
+     //
+     // }
 }
