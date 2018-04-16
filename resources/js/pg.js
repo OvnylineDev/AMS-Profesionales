057b6a1dea3fa66d714154bf240f385ffd9b0f4b
OvnylineDev <ovnylinedesarrollo@gmail.com>
2018-04-16 17:11:29 +0200

    deviceready

--- resources/js/pg.js
+++ resources/js/pg.js
@@ -1,9 +1,11 @@
+var location = "";
 //events
 // Wait for device API libraries to load
-//
+
 document.addEventListener("deviceready", phonegapReady, false);
 
 function phonegapReady(){
+     location = getURLLocation();
      document.addEventListener("backbutton", onBackKeyDown, false);
 }
 
@@ -11,8 +13,73 @@ function onBackKeyDown() {
      // alert(document.URL);
      // alert(location.pathname);
      // alert(window.location.pathname);
+     // var url = window.location.pathname;
+     // var filename = url.substring(url.lastIndexOf('/')+1);
+     // alert(filename);
+     //
+     switch (location) {
+          case "index.html":{
+               alert(filename);
+               // navigator.app.exitApp();
+          }break;
+          case "principal.html":{
+               alert(filename);
+          }break;
+          case "listado.html":{
+               alert(filename);
+          }break;
+          case "menup.html":{
+               alert(filename);
+          }break;
+          case "llamada.html":{
+               alert(filename);
+          }break;
+          case "historial.html":{
+               alert(filename);
+          }break;
+          case "seguimiento.html":{
+               alert(filename);
+          }break;
+          case "foto.html":{
+               alert(filename);
+          }break;
+          case "planos.html":{
+               alert(filename);
+          }break;
+          case "parte.html":{
+               alert(filename);
+          }break;
+          case "fincita.html":{
+               alert(filename);
+          }break;
+          case "val-foto.html":{
+               alert(filename);
+          }break;
+          case "val-general.html":{
+               alert(filename);
+          }break;
+          case "val-texto.html":{
+               alert(filename);
+          }break;
+          case "fest-eligeparte.html":{
+               alert(filename);
+          }break;
+          case "fest-foto.html":{
+               alert(filename);
+          }break;
+          case "fest-general.html":{
+               alert(filename);
+          }break;
+          case "fest-historial.html":{
+               alert(filename);
+          }break;
+          case "fest-parte.html":{
+               alert(filename);
+          }break;
+          case "fest-planos.html":{
+               alert(filename);
+          }break;
+          default:
 
-     var url = window.location.pathname;
-     var filename = url.substring(url.lastIndexOf('/')+1);
-     alert(filename);
+     }
 }
