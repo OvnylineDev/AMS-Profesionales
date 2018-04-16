77e2852d5823eba12d1d507c3bf2ecc61de2b552
OvnylineDev <ovnylinedesarrollo@gmail.com>
2018-04-16 12:56:35 +0200

    phonegapReady

--- resources/js/pg.js
+++ resources/js/pg.js
@@ -1,5 +1,11 @@
 //events
-document.addEventListener("backbutton", onBackKeyDown, false);
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready", phonegapReady, false);
+
+function phonegapReady(){
+     document.addEventListener("backbutton", onBackKeyDown, false);
+}
 
 function onBackKeyDown() {
      alert("ha pulsado back");
